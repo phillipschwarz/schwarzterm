b58c54406df5744ec9fc189b176f82c0d0c0585e
phillipschwarz <info@phillipschwarz.de>
2026-03-17 14:25:17 +0100

    Initial Commit

---
+++ schwarzterm/schwarztermApp.swift
@@ -0,0 +1,17 @@
+//
+//  schwarztermApp.swift
+//  schwarzterm
+//
+//  Created by Phillip Schwarz on 17.03.26.
+//
+
+import SwiftUI
+
+@main
+struct schwarztermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
